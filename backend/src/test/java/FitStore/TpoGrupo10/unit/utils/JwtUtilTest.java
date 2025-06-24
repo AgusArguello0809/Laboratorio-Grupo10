b00277309f1757f246da6e257616e4d8aa6899e6
Taiel Vinograd <taielvinograd@gmail.com>
2025-06-24 11:07:40 -0300

    Jwt utils updated

--- backend/src/test/java/FitStore/TpoGrupo10/unit/utils/JwtUtilTest.java
+++ backend/src/test/java/FitStore/TpoGrupo10/unit/utils/JwtUtilTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class JwtUtilTest {
@@ -16,24 +15,33 @@ class JwtUtilTest {
     void setUp() {
         jwtUtil = new JwtUtil();
         ReflectionTestUtils.setField(jwtUtil, "secretKeyRaw", "EstaEsUnaClaveSecretaJwtDeAlMenos32Caracteres!");
-        jwtUtil.init(); // Simula @PostConstruct
+        jwtUtil.init();
     }
 
     @Test
     void generateToken_deberiaRetornarTokenValido() {
-        String token = jwtUtil.generateToken("agus");
+        String token = jwtUtil.generateToken("agus", 123L);
 
         assertNotNull(token);
-        assertTrue(token.length() > 20); // Aprox largo mínimo esperado
+        assertTrue(token.length() > 20); // Largo estimado
     }
 
     @Test
     void extractUsername_conTokenValido_devuelveUsername() {
-        String token = jwtUtil.generateToken("agus");
+        String token = jwtUtil.generateToken("agus", 123L);
 
         String username = jwtUtil.extractUsername(token);
 
         assertEquals("agus", username);
+    }
+
+    @Test
+    void extractUserId_conTokenValido_devuelveId() {
+        String token = jwtUtil.generateToken("agus", 123L);
+
+        Long id = jwtUtil.extractUserId(token);
+
+        assertEquals(123L, id);
     }
 
     @Test
@@ -47,7 +55,7 @@ class JwtUtilTest {
 
     @Test
     void isTokenValid_conTokenValido_devuelveTrue() {
-        String token = jwtUtil.generateToken("agus");
+        String token = jwtUtil.generateToken("agus", 123L);
 
         assertTrue(jwtUtil.isTokenValid(token));
     }
